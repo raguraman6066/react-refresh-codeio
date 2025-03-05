8a24c71657b83f7adc570f1bcdd579a91be76f6e
ragu raman <raguraman@gmail.com>
2025-03-05 01:36:31 -0800

    filter and sort

--- src/CourseList.jsx
+++ src/CourseList.jsx
@@ -3,20 +3,33 @@ import HTML from "./assets/download.png";
 import { Course } from "./Course";
 export const CourseList = () => {
   const courses = [
-    { name: "html course", price: "$122", rating: 5, image: HTML },
+    { id: 1, name: "html course", price: 122, rating: 5, image: HTML },
     {
+      id: 2,
       name: "css course",
-      price: "$150",
+      price: 150,
       rating: 4,
       image:
         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpXaDo9xQfNOh-0nAtgdMzekEMdTbESQoFhQ&s",
     },
-    { name: "js course", price: "$200", rating: 5, image: HTML },
-    { name: "bootstrap course", price: "$500", rating: 4, image: HTML },
+    { id: 4, name: "bootstrap course", price: 500, rating: 4, image: HTML },
+    { id: 3, name: "js course", price: 200, rating: 5, image: HTML },
   ];
 
-  const courseList = courses.map((e) => (
-    <Course name={e.name} image={e.image} price={e.price} rating={e.rating} />
+  courses.sort((a, b) => {
+    return b.price - a.price;
+  });
+
+  const vfmcourses = courses.filter((e) => e.price > 200);
+
+  const courseList = vfmcourses.map((e, index) => (
+    <Course
+      key={index}
+      name={e.name}
+      image={e.image}
+      price={e.price}
+      rating={e.rating}
+    />
   ));
 
   return <>{courseList}</>;
